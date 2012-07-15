77e3159b203e0fb1ac3abe712f544643e5fd7485
Mikhail Dektyarev <mihail.dektyarow@gmail.com>
2012-07-15 14:27:39 +0200

    Exception on initialization error

--- jquery.html5_upload.js
+++ jquery.html5_upload.js
@@ -207,34 +207,40 @@
             return true;
         }
 
-        return this.each(function() {
-            this.html5_upload = {
-                xhr:                    new XMLHttpRequest(),
-                continue_after_abort:    true
-            };
-            if (options.autostart) {
-                $(this).bind('change', upload);
-            }
-            for (event in available_events) {
-                if (options[available_events[event]]) {
-                    $(this).bind("html5_upload."+available_events[event], options[available_events[event]]);
+        try {
+            return this.each(function() {
+                this.html5_upload = {
+                    xhr:                    new XMLHttpRequest(),
+                    continue_after_abort:    true
+                };
+                if (options.autostart) {
+                    $(this).bind('change', upload);
                 }
-            }
-            $(this)
-                .bind('html5_upload.start', upload)
-                .bind('html5_upload.cancelOne', function() {
-                    this.html5_upload['xhr'].abort();
-                })
-                .bind('html5_upload.cancelAll', function() {
-                    this.html5_upload['continue_after_abort'] = false;
-                    this.html5_upload['xhr'].abort();
-                })
-                .bind('html5_upload.destroy', function() {
-                    this.html5_upload['continue_after_abort'] = false;
-                    this.xhr.abort();
-                    delete this.html5_upload;
-                    $(this).unbind('html5_upload.*').unbind('change', upload);
-                });
-        });
+                for (event in available_events) {
+                    if (options[available_events[event]]) {
+                        $(this).bind("html5_upload."+available_events[event], options[available_events[event]]);
+                    }
+                }
+                $(this)
+                    .bind('html5_upload.start', upload)
+                    .bind('html5_upload.cancelOne', function() {
+                        this.html5_upload['xhr'].abort();
+                    })
+                    .bind('html5_upload.cancelAll', function() {
+                        this.html5_upload['continue_after_abort'] = false;
+                        this.html5_upload['xhr'].abort();
+                    })
+                    .bind('html5_upload.destroy', function() {
+                        this.html5_upload['continue_after_abort'] = false;
+                        this.xhr.abort();
+                        delete this.html5_upload;
+                        $(this).unbind('html5_upload.*').unbind('change', upload);
+                    });
+            });
+        }
+        catch (ex) {
+            options.onBrowserIncompatible();
+            return false;
+        }
     };
 })(jQuery);
