b693990cb521c49174a59f291a582e8dd7767655
Mikhail Dektyarev <mihail.dektyarow@gmail.com>
2015-12-12 17:51:39 +0300

    consider 200..204 statuses as success

--- jquery.html5_upload.js
+++ jquery.html5_upload.js
@@ -112,7 +112,7 @@
                     options.setProgress(options.genProgress(rpe.loaded, rpe.total));
                 };
                 xhr.onload = function(load) {
-                    if (xhr.status != 200) {
+                    if (xhr.status >= 200 || xhr.status < 205) {
                         $this.triggerHandler('html5_upload.onError', [get_file_name(file), load]);
                         if (!options.stopOnFirstError) {
                             upload_file(number+1);
